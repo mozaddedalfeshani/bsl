bcdaafd83367c1b900216f2cd72caae664047ad1
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-08-26 19:24:53 +0600

    adding list brand and machine name

--- lib/src/objective/model.dart
+++ lib/src/objective/model.dart
@@ -8,4 +8,51 @@ class TableData {
 List<TableData> tableInformation = [
   TableData("Machine Type", "Brand", "Quantity"),
   TableData("Single needle locksticth", "Juki", "420"),
+  TableData(
+      "Single needle auto fed locksticth with autoturn", "Juki", "Quantity"),
+  TableData("Twins needle fed locksticth", "Juki", "Quantity"),
+  TableData("Twins needle heavy duty locksticth", "Juki", "Quantity"),
+  TableData("Single needle doubble chainsticth", "Juki", "Quantity"),
+  TableData("Twin needle doubble chainsticth", "Juki", "Quantity"),
+  TableData("Four needle doubble chainsticth", "Juki", "Quantity"),
+  TableData("Twin needle 5 thread overlocker-2 loop", "Juki", "Quantity"),
+  TableData("Twin needle 5 thread overlocker-3 loop", "Juki", "Quantity"),
+  TableData("Locksticth buttonholer", "Juki", "Quantity"),
+  TableData("Locksticth button attachment", "Juki", "Quantity"),
+  TableData(
+      "Single needle locksticth verticle edge trimmer", "Juki", "Quantity"),
+  TableData("Bartacking machine", "Juki", "Quantity"),
+  TableData("Puller", "Juki", "Quantity"),
+  TableData("Automatic heat transfer press", "Juki", "Quantity"),
+  TableData("Cloth cutting machine", "Juki", "Quantity"),
+  //new 2 different brand
+  TableData("Conveyor type needle detector", "Hasima", "Quantity"),
+  TableData("Cuff coller fusion machine", "Kannegresser", "Quantity"),
+
+  //fully new others
+  TableData("Cutting table", "Nagai Shing", "Quantity"),
+  TableData("Cloth inspection machine", "Nagai Shing", "Quantity"),
+  TableData("Band knife cutting machine", "Nagai Shing", "Quantity"),
+  TableData("Automatic collar blocking", "Nagai Shing", "Quantity"),
+  TableData("Automatic cuff blocking", "Nagai Shing", "Quantity"),
+  TableData("Coller contour trimmer", "Nagai Shing", "Quantity"),
+  TableData("Sleeve placket fusing rotary press", "Nagai Shing", "Quantity"),
+  TableData("Automatic shirts armhole press", "Nagai Shing", "Quantity"),
+  TableData("Automatic ladies shirts armhole press", "Nagai Shing", "Quantity"),
+  TableData("Automatic sleeve sidearm press", "Nagai Shing", "Quantity"),
+  TableData("Automatic movable platform shirt placket fusion press",
+      "Nagai Shing", "Quantity"),
+  TableData("Coller creasing setting press", "Nagai Shing", "Quantity"),
+  TableData("Thread cleaning machine", "Nagai Shing", "Quantity"),
+  TableData(
+      "Stream-heated iron with 2pcs 3M teflon", "Nagai Shing", "Quantity"),
+  TableData("Pheunnatic shirt folding table", "Nagai Shing", "Quantity"),
+  TableData("Strapping machine", "Nagai Shing", "Quantity"),
+  TableData("Fusable spot welding machine", "Nagai Shing", "Quantity"),
+  TableData("Placket fusion press", "Nagai Shing", "Quantity"),
+  TableData("Button-coller reinforcement punching & sewing machine",
+      "Nagai Shing", "Quantity"),
+  TableData("Triangular sleeve placket holder", "Nagai Shing", "Quantity"),
+  TableData(
+      "Metal snap strength meastring apparafus", "Nagai Shing", "Quantity"),
 ];
