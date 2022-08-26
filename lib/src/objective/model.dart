45d566244c44b48144c9b8fd5a480ba8d724fffe
saminsohag <sssohag172@gmail.com>
2022-08-26 12:52:09 +0600

    products page edited

---
+++ lib/src/objective/model.dart
@@ -0,0 +1,18 @@
+class TableData {
+  TableData(this.machineType, this.brand, this.quantity);
+  final String machineType;
+  final String brand;
+  final String quantity;
+}
+
+List<TableData> tableDatas = [
+  TableData("Machine type", "hellow", "100"),
+  TableData("Machine type", "hellow", "100"),
+  TableData("Machine type", "hellow", "100"),
+  TableData("Machine type", "hellow", "100"),
+  TableData("Machine type", "hellow", "100"),
+  TableData("Machine type", "hellow", "100"),
+  TableData("Machine type", "hellow", "100"),
+  TableData("Machine type", "hellow", "100"),
+  TableData("Machine type", "hellow", "100"),
+];
