69cf8886880f8117bfbf4b65d6e5b0d726d079c3
saminsohag <sssohag172@gmail.com>
2022-09-14 16:10:44 +0600

    product list changes

--- lib/src/products/products_list.dart
+++ lib/src/products/products_list.dart
@@ -1,9 +1,20 @@
 import 'package:bsl/utils/constants.dart';
 
 class Products {
-  final List<Product> _kids = [];
+  final List<Product> _kids = List<Product>.generate(
+    22, // number = kids number.
+    (index) => Product(
+      "https://$imageUrl/Ladis_dress_git/Picture-${index + 1}.jpg",
+    ),
+  );
+
   List<Product> get kids => _kids;
-  final List<Product> _menS = [];
+  final List<Product> _menS = List<Product>.generate(
+    22, // number = mens number.
+    (index) => Product(
+      "https://$imageUrl/Ladis_dress_git/Picture-${index + 1}.jpg",
+    ),
+  );
 
   List<Product> get menS => _menS;
   final List<Product> _womenS = List<Product>.generate(
